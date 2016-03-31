f9e2793c631d94ff60345de7f627c78e34fbb635
Jackson Sierra <jks718@gmail.com>
2016-03-31 02:19:55 -0700

    [feature] (api/models.py) add Stats model to API app

--- api/models.py
+++ api/models.py
@@ -21,3 +21,27 @@ class Schedule(models.Model):
     def __unicode__(self):
         return self.url
 
+
+class Stats(models.Model):
+    created_date = models.DateTimeField(auto_now_add=True)
+    minutes_elapsed = models.PositiveSmallIntegerField(null=True)
+    seconds_elapsed = models.PositiveIntegerField(null=True)
+    minutes_played = models.PositiveSmallIntegerField(null=True)
+    seconds_played = models.PositiveSmallIntegerField(null=True)
+    field_goals_made = models.PositiveSmallIntegerField(null=True)
+    field_goals_attempted = models.PositiveSmallIntegerField(null=True)
+    three_pointers_made = models.PositiveSmallIntegerField(null=True)
+    three_pointers_attempted = models.PositiveSmallIntegerField(null=True)
+    free_throws_made = models.PositiveSmallIntegerField(null=True)
+    free_throws_attempted = models.PositiveSmallIntegerField(null=True)
+    plus_minus = models.CharField(max_length=20, null=True)
+    offensive_rebounds = models.PositiveSmallIntegerField(null=True)
+    defensive_rebounds = models.PositiveSmallIntegerField(null=True)
+    total_rebounds = models.PositiveSmallIntegerField(null=True)
+    assists = models.PositiveSmallIntegerField(null=True)
+    turnovers = models.PositiveSmallIntegerField(null=True)
+    steals = models.PositiveSmallIntegerField(null=True)
+    blocks = models.PositiveSmallIntegerField(null=True)
+    blocks_against = models.PositiveSmallIntegerField(null=True)
+    personal_fouls = models.PositiveSmallIntegerField(null=True)
+    points = models.PositiveSmallIntegerField(null=True)
