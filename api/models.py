e96b5160cd62d58dda4b1127345de611ff0aae9c
Jackson Sierra <jks718@gmail.com>
2016-03-30 23:53:36 -0700

    [feature] (api/models.py) add DeviceToken model to API app

--- api/models.py
+++ api/models.py
@@ -1,5 +1,10 @@
 from __future__ import unicode_literals
-
 from django.db import models
 
-# Create your models here.
+
+class DeviceToken(models.Model):
+    token = models.CharField(max_length=256, unique=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+
+    def __unicode__(self):
+        return self.token
