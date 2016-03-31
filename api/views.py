7f0925fbd27ed69d059a4666d16f10f573eae2af
Jackson Sierra <jks718@gmail.com>
2016-03-30 23:47:17 -0700

    [feature] (api/views.py) add corresponding Views for api/register/ and api/loadstate/ routes

--- api/views.py
+++ api/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+import json
 
-# Create your views here.
+from api.models import DeviceToken
+from api.utils import calculate_state
+
+
+@csrf_exempt
+def register(request):
+    if request.method == 'POST':
+        body = json.loads(request.body)
+        try:
+            token, created = DeviceToken.objects.get_or_create(token=body['device_token'])
+        except DeviceToken.DoesNotExist:
+            return HttpResponse(content=sys.exc_info()[0], status=500)
+
+        if created:
+            return HttpResponse(status=201)
+
+        return HttpResponse(status=202)
+
+    return HttpResponse(status=400)
+
+
+def load_state(request):
+    state = calculate_state()
+    return HttpResponse(content=json.dumps({'type': state}), content_type='application/json')
